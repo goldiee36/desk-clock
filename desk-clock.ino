f27a9a5b260e11254cdfd8afdacbc5ed9af9a04a
goldiee36 <boldizsar.adam@gmail.com>
2017-05-19 08:00:56 +0200

    vcorr pink added

--- desk-clock.ino
+++ desk-clock.ino
@@ -39,7 +39,8 @@ U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_NO_ACK|U8G_I2C_OPT_FAST)
 //vcc livingRoom
 // Measured Vcc by multimeter divided by reported Vcc
 #define VCORR 3.5/3.5;
-const float VccCorrection = VCORR; //todo why float
+#define VCORRPINK 4.058/3.93;
+const float VccCorrection = VCORRPINK; //todo why float
 Vcc vcc(VccCorrection);
 
 
